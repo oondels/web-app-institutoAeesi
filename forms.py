c9141b12a0b87afd0a21be7741c88ad5ac66f409
oondels <hendriusfelix@gmail.com>
2024-03-12 19:44:30 -0300

    cadastro telefone

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, RadioField
-from wtforms import validators
-
+from wtforms.validators import DataRequired, Length
+cursos = [("Jiu-Jitsu","Jius-Jitsu"), ("RitBox", "RitBox"), ("Box", "Box"), ("Academia", "Academia")]
 class Cadastro_Form(FlaskForm):
-    nome = StringField("Nome:", validators=[validators.DataRequired()])
-    data_nascimento = StringField("Data Nascimento:", validators=[validators.Length(min=6, max=35)])
-    curso = StringField("Curso:", validators=[validators.DataRequired()])
-    idade = StringField("Idade:", validators=[validators.DataRequired(), validators.Length(min=1, max=3)])
+    nome = StringField("Nome:", validators=[DataRequired()])
+    idade = StringField("Idade:", validators=[DataRequired(), Length(min=1, max=3)])
+    telefone = StringField("Telefone: ", validators=[DataRequired(), Length(min=9, max=14)])
+    curso = RadioField("Curso:", choices=cursos)
     submit = SubmitField("Cadastrar")
