1ace8f04dac656d54af6f37c1a64394486997ec8
oondels <hendriusfelix@gmail.com>
2024-04-05 22:35:20 -0300

    js file to put search script

---
+++ static/js/search_aluno.js
@@ -0,0 +1,19 @@
+function search_aluno() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('input-pesquisa');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("aluno-pesquisa");
+    li = ul.getElementsByTagName('li');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
