b5f7c215b412d73aa47a504e396be3e302bdae7f
oondels <hendriusfelix@gmail.com>
2024-04-10 20:58:14 -0300

    Teste de exibição "Mostrar mais" -> admin page

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,8 @@ let verMais = document.getElementById("ver-mais");
 let mostrarMais = document.getElementById("mostrar-todos");
 
 const mostrarMaisAlunos = () => {
-    verMais.style.display = 'block';
+    verMais.style.display = 'inline';
+    
 };
 
 mostrarMais.addEventListener('click', mostrarMaisAlunos);
