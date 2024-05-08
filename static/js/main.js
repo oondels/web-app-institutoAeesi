46b3be13ed48a2498c4437ef2e5fcb190904d58d
oondels <hendriusfelix@gmail.com>
2024-05-08 11:25:13 -0300

    remção mainjs

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-let itemToshow = document.getElementsByClassName("itens-toShow")[0];
-let buttonShow = document.getElementsByClassName("mostrar-itens")[0];
-
-const mostrarMaisAlunos = () => {
-    itemToshow.style.display = 'block';
-    
-};
-
-buttonShow.addEventListener('click', mostrarMaisAlunos);
