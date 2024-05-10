df4506ec4886fb407414e0e35e5ccc247c7cd694
oondels <hendriusfelix@gmail.com>
2024-05-10 01:02:15 -0300

    heroku

--- teste.py
+++
@@ -1,9 +0,0 @@
-from datetime import date, datetime
-
-mes_pagamento = "03-04-2024"
-
-format = "%d-%m-%Y"
-if datetime.strptime(mes_pagamento, format):
-    print(mes_pagamento)
-else:
-    print("error")
