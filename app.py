4d86a176653a288bbd015804fa615a205c84f460
oondels <hendriusfelix@gmail.com>
2024-05-11 13:00:20 -0300

    sql update

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ db = SQLAlchemy(app)
 
 app.config['UPLOAD_FOLDER'] = folder
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(path, 'database/geral.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ubldlai8g393bf:p252aa443b6c334ea727f23092bd7138a715707651a8b415a1230355746683c12@cb5ajfjosdpmil.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dafm2p9faq3kmb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
