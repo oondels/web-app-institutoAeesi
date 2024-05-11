b80e5a5982de589ad1cd93e1810cd3950dfc7e40
oondels <hendriusfelix@gmail.com>
2024-05-11 13:28:44 -0300

    ..

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import datetime, date
 path = os.path.abspath(os.path.dirname(__file__))
 folder = os.path.join(path, "database/files")
 
-db = SQLAlchemy(Flask(__name__))
+db = SQLAlchemy()
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "wa0i4Ochu" #Editar senha depois
