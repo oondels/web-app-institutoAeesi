3c5b8a68d0c4dd5fb6048680130e76ca8c8c1877
oondels <hendriusfelix@gmail.com>
2024-05-11 10:23:39 -0300

    app heroku

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ folder = os.path.join(path, "database/files")
 
 app.config['UPLOAD_FOLDER'] = folder
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(path, 'database/geral.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kofntiovsbspst:e7cfd6445eb4ea97fc9a3f5f7bbdff3b10c5c750fd253eba72b9ac6f345b3371@ec2-44-215-176-210.compute-1.amazonaws.com:5432/d72r86ju5ro7k'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://sywbfrfepbxmmb:7c151d8ee719755d1d05c90e6e251fd952d1b536da569fb6d635a785261fe191@ec2-52-23-12-61.compute-1.amazonaws.com:5432/d76n6ndo9mji2m'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 login_manager = LoginManager()
