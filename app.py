8b8b2455ec5cf44d0537da0d486b140b2adf7baf
oondels <hendriusfelix@gmail.com>
2024-05-12 10:53:41 -0300

    extension db

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 import os
 from flask import Flask, render_template, redirect, url_for, flash, request
 from forms import Cadastro_Form, Upload_File, Register_User, Login_User, Editar_Form
+from extension import db
 from werkzeug.utils import secure_filename
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, LoginManager, login_required, login_user, current_user, logout_user
 from datetime import datetime, date
-from flask_migrate import Migrate
-from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 
 path = os.path.abspath(os.path.dirname(__file__))
 folder = os.path.join(path, "database/files")
 
-# with app.app_context(): 
-#         db.create_all()
 app.config["SECRET_KEY"] = "wa0i4Ochu"
 app.config['UPLOAD_FOLDER'] = folder
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(path, 'database/geral.db')
@@ -24,9 +20,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_RECORD_QUERIES"] = True
 
-db = SQLAlchemy()
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
+db.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
