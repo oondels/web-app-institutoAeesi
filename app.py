f94118d58c579828be12886ef675bd11af0317bf
oondels <hendriusfelix@gmail.com>
2024-05-13 09:05:42 -0300

    mysql

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, LoginManager, login_required, login_user, current_user, logout_user
 from datetime import datetime, date
 from dotenv import load_dotenv
-from flask_sqlalchemy import SQLAlchemy
+
 
 load_dotenv()
 
@@ -19,11 +19,11 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "wa0i4Ochu"
 app.config["UPLOAD_FOLDER"] = folder
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(path, 'database/geral.db')
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(path, 'database/geral.db')
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
